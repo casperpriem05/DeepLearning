263b14f1e3b7d5da0a140764d437fc2e069c179e
Lars Timmer <lars.l.timmer@gmail.com>
2025-06-19 16:19:41 +0200

    model 2 bezig (nog niet werkend)

--- Opdracht 3/install.bash
+++ Opdracht 3/install.bash
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+pip install accelerate
+pip install bitsandbytes
+pip install datasets 
+pip install matplotlib 
 pip install pillow
-pip install matplotlib
-pip install torch
+pip install tensorflow 
+pip install torch 
+pip install transformers
 pip install ultralytics==8.3.136
 pip uninstall -y opencv-python
 pip install opencv-python-headless==4.10.0.84
+pip install --upgrade "numpy<2.0"
+pip install --upgrade "optree>=0.13.0"
