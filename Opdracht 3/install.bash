8f94b1714a4dca55d09d0f121fb72c0d3683caea
Lars Timmer <lars.l.timmer@gmail.com>
2025-06-19 16:45:25 +0200

    Opdracht 4

--- Opdracht 3/install.bash
+++ Opdracht 3/install.bash
@@ -3,11 +3,13 @@
 pip install accelerate
 pip install bitsandbytes
 pip install datasets 
+pip install diffusers
 pip install matplotlib 
 pip install pillow
 pip install tensorflow 
 pip install torch 
 pip install transformers
+pip install xformers
 pip install ultralytics==8.3.136
 pip uninstall -y opencv-python
 pip install opencv-python-headless==4.10.0.84
