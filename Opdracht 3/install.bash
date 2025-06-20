58768b77715d6828f3e5ed51245510e9921c1b80
laltir <lars.l.timmer@gmail.com>
2025-06-20 08:48:59 +0000

    Commits not working yet

--- Opdracht 3/install.bash
+++ Opdracht 3/install.bash
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-pip install accelerate
-pip install bitsandbytes
-pip install datasets 
-pip install diffusers
-pip install matplotlib 
-pip install pillow
-pip install tensorflow 
-pip install torch 
-pip install transformers
-pip install xformers
+python -m pip install --upgrade pip
+pip install accelerate==0.28.0
+pip install matplotlib==3.10.3
+pip install pillow==9.3.0
+pip install tensorflow==2.19.0
+pip install diffusers==0.26.3 bitsandbytes==0.42.0
+pip install transformers==4.38.2 diffusers==0.26.3 datasets==2.18.0 huggingface-hub==0.21.4
+pip install xformers==0.0.25.post1 --index-url https://download.pytorch.org/whl/cu121
+pip install torch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 --index-url https://download.pytorch.org/whl/cu121
 pip install ultralytics==8.3.136
 pip uninstall -y opencv-python
 pip install opencv-python-headless==4.10.0.84
